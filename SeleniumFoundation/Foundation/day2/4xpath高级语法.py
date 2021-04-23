013dd49dd3e64d432f20c08b3c11fe7ff577dee6
jilong <jilong930323@163.com>
2021-04-23 17:08:03 +0800

    2021年4月23日11点30分提交显示等待/xpath高级语法

--- SeleniumFoundation/Foundation/day2/4xpath高级语法.py
+++ SeleniumFoundation/Foundation/day2/4xpath高级语法.py
@@ -7,8 +7,13 @@ driver = webdriver.Chrome()
 # 访问网址
 driver.get('file:///E:/workspase/pythonProject/PythonSelenium/SeleniumFoundation/Foundation/day2/test.html')
 
-elenium = driver.find_element_by_xpath('//tr[2]//td[3]')
-print(elenium.text)
+elenium = driver.find_element_by_xpath("//tr//td[@id='a1']/..")
+'''
+//tr//td[@id='a1']/parent::tr #parent::等同于/.. 父元素
+//tr//td[@id='a1']/ancestor-or-self::td #ancestor-or-self::*包含上下文节点本身和该节点的祖先节点
+descendant 选取当前节点的后代元素
+'''
+print(elenium.find_element_by_xpath('./td[3]').text)
 
 
 # 关闭浏览器
